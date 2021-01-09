2bdf7d5028e4a2b4b1e6ba2359331da7749751e6
shaggy <simmons5family@gmail.com>
2021-01-08 23:47:03 -0600

    put => create new Category / POST => update category name

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -13,12 +13,38 @@ router.get('/:id', (req, res) => {
   // be sure to include its associated Products
 });
 
+  // CREATE new Category    WORKS
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+    .then(dbData => res.json(dbData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err)
+    });
 });
-
+  // UPDATE a category by its `id` value  WORKS
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  ).then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' })
+            return;
+        }
+        res.json(dbPostData);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    });
 });
 
 router.delete('/:id', (req, res) => {
